37f5bd1f4d0008d3897f613ceca70193d75a9a13
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2016-09-05 16:18:00 +0200

    add all tests

--- src/GlLazyLoadImg.php
+++ src/GlLazyLoadImg.php
@@ -18,53 +18,168 @@
  *
  */
 
-namespace GLICER\Solver\Command;
+namespace GlLazyLoadImg;
 
 use GlHtml\GlHtml;
 
 class GlLazyLoadImg
 {
+    const BLANK = 0;
+    const LOSSY = 1;
+
     /**
      * @var string rootpath
      */
     private $rootpath;
-    
-    public function __construct($rootpath) {
+
+    /**
+     * constructor - set root directory to relative url
+     *
+     * @param string $rootpath
+     */
+    public function __construct($rootpath)
+    {
         $this->rootpath = $rootpath;
     }
 
     /**
-     * @param $html
+     * create lossy image and encode to data uri format
+     * minimal size is jpeg
      *
+     * @param     $src          resource GD library
+     * @param int $quality      jpeg quality from 0 (poor quality) to 100 (best quality) - default 0
+     *
+     * @return string
+     */
+    public function getLossyDataURI($src, $quality = 0)
+    {
+        ob_start();
+        imagejpeg($src, null, $quality);
+        $data = ob_get_contents();
+        ob_end_clean();
+
+        $base64 = base64_encode($data);
+
+        $mime = 'image/jpeg';
+
+        return ('data:' . $mime . ';base64,' . $base64);
+    }
+
+    /**
+     * create blank image with same size in data uri format
+     * minimal size is gif
+     *
+     * @param     $src          resource GD library
+     * @param int $red          red component background color (default 255)
+     * @param int $green        green component background color (default 255)
+     * @param int $blue         blue component background color (default 255)
+     *
+     * @return string
+     */
+    public function getBlankDataURI($src, $red = 255, $green = 255, $blue = 255)
+    {
+        $width  = imagesx($src);
+        $height = imagesy($src);
+
+        $img   = imagecreatetruecolor($width, $height);
+        $bgcol = imagecolorallocate($img, $red, $green, $blue);
+        imageFill($img, 0, 0, $bgcol);
+
+        ob_start();
+        imagegif($img);
+        $data = ob_get_contents();
+        ob_end_clean();
+
+        $base64 = base64_encode($data);
+
+        imagedestroy($img);
+
+        $mime = 'image/gif';
+
+        return ('data:' . $mime . ';base64,' . $base64);
+    }
+   
+    /**
+     * replace all src attributes from img tags with datauri and set another attribute with old src value
+     * support jpeg, png or gif file format
+     *
+     * @param string $html
+     * @param int    $type                  (self::BLANK=0 or self::LOSSY=1)
+     * @param string $moveToAttribute       move src value to $attribute (default = data-original)
+     * @param array  $excludeAttributesList list of attributes to excludes
+     *
+     * @throws \Exception
+     */
+    public function autoDataURI(
+        $html,
+        $type = self::BLANK,
+        $moveToAttribute = 'data-original',
+        array $excludeAttributesList = []
+    ) {
+        $html = new GlHtml($html);
+        $imgs = $html->get('img');
+        foreach ($imgs as $img) {
+            $src          = $img->getAttribute('src');
+            $pathimagesrc = $this->rootpath . '/' . $src;
+
+            $imgbin = @imagecreatefromjpeg($pathimagesrc);
+            if (!$imgbin) {
+                $imgbin = @imagecreatefrompng($pathimagesrc);
+                if (!$imgbin) {
+                    $imgbin = @imagecreatefromgif($pathimagesrc);
+                }
+            }
+
+            if ($imgbin) {
+                switch ($type) {
+                    case self::BLANK:
+                        $datauri = $this->getBlankDataURI($imgbin);
+                        break;
+                    case self::LOSSY:
+                        $datauri = $this->getLossyDataURI($imgbin);
+                        break;
+                    default:
+                        throw new \Exception("Type unknown (only self::BLANK=0 or self::LOSSY=1 accepted) : " . $type);
+                }
+
+                
+                if (!$img->hasAttributes($excludeAttributesList)) {
+                    $img->setAttributes([$moveToAttribute => $src, 'src' => $datauri]);
+                }
+            }
+        }
+        
+        return $html->html();
+    }
+
+    /**
+     * @param string $html
+     * @param array  $excludeAttributesList
+     * 
      * @throws \Exception
      * @return string
      */
-    public function set($html)
+    public function autoWidthHeight($html,array $excludeAttributesList = [])
     {
         $html = new GlHtml($html);
         $imgs = $html->get('img');
         foreach ($imgs as $img) {
-            $src = $img->getAttribute('src');
-            if (($src != '#') && ($src != '/nojavascript.php')) {
-                $pathimagesrc = $this->rootpath . '/' . $src;
-                $imgbin       = @imagecreatefromjpeg($pathimagesrc);
+            $src          = $img->getAttribute('src');
+            $pathimagesrc = $this->rootpath . '/' . $src;
+            $imgbin       = @imagecreatefromjpeg($pathimagesrc);
+            if (!$imgbin) {
+                $imgbin = @imagecreatefrompng($pathimagesrc);
                 if (!$imgbin) {
-                    $imgbin = @imagecreatefrompng($pathimagesrc);
-                    if (!$imgbin) {
-                        $imgbin = @imagecreatefromgif($pathimagesrc);
-                        if (!$imgbin) {
-                            throw new \Exception('Cannot read img : ' . $src);
-                        }
-                    }
+                    $imgbin = @imagecreatefromgif($pathimagesrc);
                 }
-                if ($imgbin) {
-                    $width  = imagesx($imgbin);
-                    $height = imagesy($imgbin);
-                    imagedestroy($imgbin);
-                    $img->setAttributes(['width' => $width, 'height' => $height]);
-                }
+            }
+            if ($imgbin) {
+                $width  = imagesx($imgbin);
+                $height = imagesy($imgbin);
+                imagedestroy($imgbin);
+                $img->setAttributes(['width' => $width, 'height' => $height]);
 
-                if (!$img->getAttribute('nolazyload') && !$img->getAttribute('data-original')) {
+                if (!$img->hasAttributes($excludeAttributesList)) {
                     $img->setAttributes(['data-original' => $src, 'src' => '#']);
                 }
             }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+if (!file_exists(__DIR__ . '/result')) {
+    mkdir(__DIR__ . '/result');
+}
+
