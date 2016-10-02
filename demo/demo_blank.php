3dbd8f64559efd2f976a553d6700683aded50b06
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2016-10-02 13:59:34 +0200

    add lossy demo

--- demo/demo_blank.php
+++ demo/demo_blank.php
@@ -6,5 +6,5 @@ use GlLazyLoadImg\GlLazyLoadImg;
 $html = file_get_contents(__DIR__ . '/_demo3.html');
 $lazyload = new GlLazyLoadImg(__DIR__,GlLazyLoadImg::BLANK, 'data-original');
 $result = $lazyload->autoDataURI($html);
-file_put_contents(__DIR__ . '/demo3.html', $result);
+file_put_contents(__DIR__ . '/demo3_blank.html', $result);
 
---
+++ demo/demo_lossy.php
@@ -0,0 +1,10 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use GlLazyLoadImg\GlLazyLoadImg;
+
+$html = file_get_contents(__DIR__ . '/_demo3.html');
+$lazyload = new GlLazyLoadImg(__DIR__,GlLazyLoadImg::LOSSY, 'data-original');
+$result = $lazyload->autoDataURI($html);
+file_put_contents(__DIR__ . '/demo3_lossy.html', $result);
+
